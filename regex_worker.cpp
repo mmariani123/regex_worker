9f35f86145a46045c2e8dc68fec2cdf770c6ea2e
Michael Mariani <37840656+mmariani123@users.noreply.github.com>
2020-11-30 16:55:07 -0500

    regex_worker update 11/30/2020
    upadate 11/30/2020

--- regex_worker.cpp
+++ regex_worker.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <regex>
 
 using namespace std;
 
@@ -39,165 +43,84 @@ int main()
     while(runProgram==true)
     {
 
-    //https://www.geeksforgeeks.org/buffer-flush-means-c/
-    //In C++, we can explicitly flushed to forced the buffer to be written.
-    //Generally std::endl function works the same by inserting new-line
-    //character and flushes the stream. stdout/cout is line-buffered
-    //that is the output doesn’t get sent to the OS until you write
-    //a newline or explicitly flush the buffer.
-    //cout << "Search a string with regex" << flush;
+        //https://www.geeksforgeeks.org/buffer-flush-means-c/
+        //In C++, we can explicitly flushed to forced the buffer to be written.
+        //Generally std::endl function works the same by inserting new-line
+        //character and flushes the stream. stdout/cout is line-buffered
+        //that is the output doesn’t get sent to the OS until you write
+        //a newline or explicitly flush the buffer.
+        //cout << "Search a string with regex" << flush;
 
-    cout << "Welcome to Regex Worker, where you can search a string with regex" << endl;
+        cout << "Welcome to Regex Worker, where you can search a string with regex" << endl;
 
-    //string to be searched
-    //string mystr3 = "She sells_sea shells by the sea shore";
-    //enter a string to be searched
+        //string to be searched
+        //string mystr3 = "She sells_sea shells by the sea shore";
+        //enter a string to be searched
 
-    cout << "enter a string to be searched" << endl;
-    string stringInput;
-    getline(cin, stringInput);
+        cout << "enter a string to be searched" << endl;
+        string stringInput;
+        getline(cin, stringInput);
 
-    //enter a regex string
-    cout << "enter a regex pattern as a string" << endl;
-    string regexInput;
-    getline(cin, regexInput);
+        //enter a regex string
+        cout << "enter a regex pattern as a string" << endl;
+        string regexInput;
+        getline(cin, regexInput);
 
-    // regex expression for pattern to be searched
-    //regex regexp("s[a-z_]+");
-    regex regexp(regexInput);
+        // regex expression for pattern to be searched
+        //regex regexp("s[a-z_]+");
+        regex regexp(regexInput);
 
-    // flag type for determining the matching behavior (in this case on string objects)
-    // instantiate matches object for string class;
-    smatch m;
+        // flag type for determining the matching behavior (in this case on string objects)
+        // instantiate matches object for string class;
+        smatch m;
 
-    // regex_search that searches pattern regexp in the string mystr
-    regex_search(stringInput, m, regexp);
+        // regex_search that searches pattern regexp in the string mystr
+        regex_search(stringInput, m, regexp);
 
-    // "auto" performs automatic type detection for us
+        // "auto" performs automatic type detection for us
 
-    //https://stackoverflow.com/questions/35490236/colon-and-auto-in-for-loop-c-need-some-help-understanding-the-syntax
+        //https://stackoverflow.com/questions/35490236/colon-and-auto-in-for-loop-c-need-some-help-understanding-the-syntax
 
-    //This is a range based for loop, it has the same basic behavior of:
-    //for(auto it = deviceList.begin(); it != deviceList.end(); ++it)
-    //{
-    //  const auto& ioDev = *it;
-    //}
-    //The range based for loop has quickly become one of my favorite constructs,
-    //it's terse and when you need to iterate an entire range, works as well
-    //and as efficiently) as possible.
+        //This is a range based for loop, it has the same basic behavior of:
+        //for(auto it = deviceList.begin(); it != deviceList.end(); ++it)
+        //{
+        //  const auto& ioDev = *it;
+        //}
+        //The range based for loop has quickly become one of my favorite constructs,
+        //it's terse and when you need to iterate an entire range, works as well
+        //and as efficiently) as possible.
 
-    //If you need the other constructs of a typical for loop
-    //(say to exit early in some case),
-    //then range-based for isn't for that use case.
+        //If you need the other constructs of a typical for loop
+        //(say to exit early in some case),
+        //then range-based for isn't for that use case.
+        ofstream myFile;
+        myFile.open("regex_matches.txt");
+        cout << "String that matches the pattern:" << endl;
+        for(auto x : m){
+            //Output match results to console:
+            cout << x << endl;
+            //Output match results to file:
+            //Create and open a text file
+            myFile << x << "\n";
+        }
+        // Close the file
+        myFile.close();
 
-    cout << "String that matches the pattern:" << endl;
-    for (auto x : m){
-      cout << x << " " << endl;
-    }
+        string programStatus;
+        cout << "would you like to run again? type yes or no" << endl;
+        getline(cin,programStatus);
 
-    string programStatus;
-    cout << "would you like to run again? type yes or no" << endl;
-    getline(cin,programStatus);
-
-    if(programStatus=="yes"){
-        runProgram=true;
-    }else if(programStatus=="no"){
-        runProgram=false;
-    }else{
-        cout << "did not enter yes or no, program terminating" << endl;
-        cin.get();
-        break;
-    }
+        if(programStatus=="yes"){
+            runProgram=true;
+        }else if(programStatus=="no"){
+            runProgram=false;
+        }else{
+            cout << "did not enter yes or no, program terminating" << endl;
+            cin.get();
+            break;
+        }
 
     }
     return(0);
-
-
-
-    #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <algorithm>
-#include <regex>
-
-using namespace std;
-
-std::ifstream fileIn("22_S22_L002_R1_001.sorted.mapped.narrowPeak");
-std::string   line;
-
-// Create and open a text file
-std::ofstream MyFile("22_S22_L002_R1_001.sorted.mapped.bed");
-
-int main()
-{
-
- if(fileIn.is_open())
- {
-    while(std::getline(fileIn, line))
-    {
-        std::stringstream   linestream(line);
-        std::string         data;
-        //std::string         val1;
-        //std::string         val2;
-
-        // If you have truly tab delimited data use getline() with third parameter.
-        // If your data is just white space separated data
-        // then the operator >> will do (it reads a space separated word into a string).
-        std::getline(linestream, data, '\n');  // read up-to the first tab (discard tab).
-
-        // Read the integers using the operator >>
-        //linestream >> val1 >> val2;
-
-        //std::cout << data << std::endl;
-
-        //std::cout << val1 << val2 << std::endl;
-
-        std::string s = data;
-
-        if(regex_match(s, regex("^(track)(.*)"))){continue;}
-
-        std::string delimiter = "\t";
-
-        //remove newline:
-        s.erase(std::remove(s.begin(), s.end(), '\n'), s.end());
-
-            size_t pos = 0;
-            std::string token;
-            while ((pos = s.find(delimiter)) != std::string::npos) {
-            token = s.substr(0, pos);
-            std::cout << token << std::endl;
-            std::cin.get();
-            s.erase(0, pos + delimiter.length());
-            //(if you want to preserve the original string, just use s = s.substr(pos + delimiter.length());):
-            }
-            //std::cout << s << std::endl;
-            std::string arr[10];
-            int i = 0;
-            std::stringstream ssin(line);
-            while (ssin.good() && i < 10){
-                ssin >> arr[i];
-                ++i;
-            }
-            for(i = 0; i < 10; i++){
-                std::cout << arr[i] << std::endl;
-               // Write to the file
-               if(i<2){
-                MyFile << arr[i] << "\t";
-               }else if(i==2){
-               MyFile << arr[i] << std::endl;
-               }
-            }
-            std::cin.get();
-    }
-
- }
-
-
-// Close the file
-MyFile.close();
-
-return(0);
-
-}
 
 }
